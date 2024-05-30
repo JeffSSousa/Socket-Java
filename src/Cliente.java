19e6b1d4f1638e03faca5eed12b8eb6b3efaca94
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2024-05-30 09:28:01 -0300

    Adicionado Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,16 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
+	public static void main (String[]args) throws IOException{
+		
+	  Socket socket= new Socket("Localhost",4000);
+	  Scanner sc = new Scanner(System.in);
+	  PrintStream saida = new PrintStream(socket.getOutputStream());
+	  saida.println(sc.nextLine());
+	  
+	}
 
 }
